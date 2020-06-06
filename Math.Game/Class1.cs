180cb82669f8cb6853e506a720b2b9f713e7d86b
Vinoth Sabanadesan <vinoth15@gmail.com>
2020-06-06 09:34:05 -0400

    new projs game and stat

---
+++ Math.Game/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Math.Game
+{
+    public class Class1
+    {
+    }
+}
---
+++ Math.Stat/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Math.Stat
+{
+    public class Class1
+    {
+    }
+}
